1354a10ece6e87d1119ea9604114c3862c24793b
bitbyte08 <me@bitworkspace.kr>
2025-10-20 14:27:49 +0900

    delete :: 필요없는 속성 제거

--- app/models.py
+++ app/models.py
@@ -10,7 +10,7 @@ from sqlalchemy import (
     DateTime,
 )
 from sqlalchemy.orm import relationship
-from app.database import Base
+from database import Base
 
 
 # ==========================
@@ -22,7 +22,6 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     password_hash = Column(String, nullable=False)
-    role = Column(String, default="member")
     created_at = Column(TIMESTAMP, default=datetime.utcnow)
 
     # Relationships
