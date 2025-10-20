c7d58bd74cd4624fd648167c76624c00f3328181
bitbyte08 <me@bitworkspace.kr>
2025-10-20 14:31:10 +0900

    feat :: 라우터 추가

--- app/main.py
+++ app/main.py
@@ -1,7 +1,26 @@
 # main.py
-from app.database import init_db
+from fastapi import FastAPI
+from database import init_db
+from users.router import router as users_router
+from teams.router import router as teams_router
+from schedules.router import router as schedules_router
+from auth.router import router as auth_router
+import uvicorn
+
+init_db()
+app = FastAPI(title="Clocker API")
+
+# 각 도메인 router 등록
+app.include_router(users_router, prefix="/users", tags=["Users"])
+app.include_router(teams_router, prefix="/teams", tags=["Teams"])
+app.include_router(schedules_router, prefix="/schedules", tags=["Schedules"])
+app.include_router(auth_router, prefix="/auth", tags=["Auth"])
+
 
 if __name__ == "__main__":
-    print("Initializing database...")
-    init_db()
-    print("✅ Database tables created successfully.")
+    uvicorn.run(
+        "main:app",      # "파일명:FastAPI객체명"
+        host="0.0.0.0",  # 컨테이너 외부에서 접속 가능
+        port=8000,
+        reload=True      # 개발 모드에서 코드 변경 시 자동 재시작
+    )
